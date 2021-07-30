3b7993ef50cc01d128ac29d6c362504617fbf872
Bandi-Saideva-Goud <88177812+Bandi-Saideva-Goud@users.noreply.github.com>
2021-07-30 16:41:25 +0530

    Update signup.php

--- includes/signup.php
+++ includes/signup.php
@@ -33,7 +33,7 @@ and open the template in the editor.
             echo $email_exist;
         }
         ?>
-        <div class="container" style="margin-top:50px; padding-left:30%; padding-top:10%">
+        <div class="container" style="margin-top:50px; padding-left:30%; padding-top:20%">
             <div class="row">
                 <div class="col-sm-5">
                     <h1>SIGN UP</h1>
